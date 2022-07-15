11fbd94aae98e7d5c2d25ebc4d368d0a4556acd7
Mirashi12 <rqt87000@gmail.com>
2022-07-15 22:27:16 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("hey");
+function getComputerChoice() {
+    let computerRandom = Math.floor(Math.random() * 3);
+    if (computerRandom == 0) {
+        let computerChoice = "Rock";
+        console.log(`The computer chose ${computerChoice}.`);
+    } else if (computerRandom == 1) {
+        let computerChoice = "Paper";
+        console.log(`The computer chose ${computerChoice}.`);
+    } else if (computerRandom == 2) {
+        let computerChoice = "Scissor";
+        console.log(`The computer chose ${computerChoice}.`);
+    }
+}
+
+console.log("Here is a simple (R)ock, (P)aper, (S)cissor game.");
+getComputerChoice();
