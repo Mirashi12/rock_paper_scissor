0309b9935f140fdffb122c35d00a84e8b970fd64
Mirashi12 <rqt87000@gmail.com>
2022-07-15 22:53:31 +0200

    Add resolveGame function

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+let playerMove;
+let computerMove;
+
 function getComputerChoice() {
     let computerRandom = Math.floor(Math.random() * 3);
     if (computerRandom == 0) {
-        let computerChoice = "Rock";
-        console.log(`The computer chose ${computerChoice}.`);
+        computerMove = "Rock";
+        console.log(`The computer chose ${computerMove}.`);
     } else if (computerRandom == 1) {
-        let computerChoice = "Paper";
-        console.log(`The computer chose ${computerChoice}.`);
+        computerMove = "Paper";
+        console.log(`The computer chose ${computerMove}.`);
     } else if (computerRandom == 2) {
-        let computerChoice = "Scissor";
-        console.log(`The computer chose ${computerChoice}.`);
+        computerMove = "Scissor";
+        console.log(`The computer chose ${computerMove}.`);
     }
 }
 
@@ -16,17 +19,41 @@ function getPlayerSelection() {
     let playerChoice = prompt("Enter R for Rock, P for Paper, S for Scissor").toLowerCase();
     
     if (playerChoice == "r") {
-        let playerMove = "Rock";
+        playerMove = "Rock";
         console.log(`You chose ${playerMove}.`);
     } else if (playerChoice == "p") {
-        let playerMove = "Paper";
+        playerMove = "Paper";
         console.log(`You chose ${playerMove}.`);
     } else if (playerChoice == "s") {
-        let playerMove = "Scissor";
+        playerMove = "Scissor";
         console.log(`You chose ${playerMove}.`);
-    }  
+    }  else {
+        console.log("This is not a valid input.")
+    }
+}
+
+function resolveGame() {
+    if (computerMove === "Rock" && playerMove === "Paper") {
+        console.log("You win!");
+    } else if (computerMove === "Rock" && playerMove === "Scissor") {
+        console.log("The computer wins!");
+    } else if (computerMove === "Paper" && playerMove === "Rock") {
+        console.log("The computer wins!");
+    } else if (computerMove === "Paper" && playerMove === "Scissor") {
+        console.log("You win!");
+    } else if (computerMove === "Scissor" && playerMove === "Rock") {
+        console.log("You win!");
+    } else if (computerMove === "Scissor" && playerMove === "Paper") {
+        console.log("The computer wins!")
+    } else if (computerMove === playerMove) {
+        console.log("This is a Draw!");
+    } else {
+        console.log("The game could not be played, enter a valid input.")
+    }
 }
 
 console.log("Here is a simple (R)ock, (P)aper, (S)cissor game.");
-getComputerChoice();
+
 getPlayerSelection();
+getComputerChoice();
+resolveGame();
